316e402b07e242d104b496e22074fd338d78a805
Tobias Fankhänel <tfankhaenel@codevise.de>
2016-07-05 21:06:37 +0200

    Add highdef video definitions for Zencoder
    Added definitions are fullhd (1080p) und 4k (2160p).
    
    Outputs will be skipped if skip_highdef is true.
    
    Highdef outputs are only generated if source resolution exceeds
    min_size.

--- lib/pageflow/zencoder_video_output_definition.rb
+++ lib/pageflow/zencoder_video_output_definition.rb
@@ -1,6 +1,6 @@
 module Pageflow
   class ZencoderVideoOutputDefinition < ZencoderOutputDefinition
-    cattr_accessor :skip_hls, :skip_smil
+    cattr_accessor :skip_hls, :skip_smil, :skip_highdef
 
     attr_reader :video_file
 
@@ -26,6 +26,9 @@ module Pageflow
         thumbnails_definitions
       ]
       returned_outputs += non_transferable(smil_definition) unless skip_smil
+      unless skip_highdef
+        returned_outputs += [transferable(mp4_4k_definition), transferable(mp4_fullhd_definition)]
+      end
       returned_outputs.flatten
     end
 
@@ -33,79 +36,111 @@ module Pageflow
 
     def webm_high_definition
       {
-        :label => 'webm_high',
-        :format => 'webm',
-        :path => video_file.webm_high.path,
-        :size => '1280x720',
-        :quality => 4,
-        :max_video_bitrate => 3500,
-        :public => 1,
-        :sharpen => true
+        label: 'webm_high',
+        format: 'webm',
+        path: video_file.webm_high.path,
+        size: '1280x720',
+        quality: 4,
+        max_video_bitrate: 3500,
+        public: 1,
+        sharpen: true
       }
     end
 
     def webm_medium_definition
       {
-        :label => 'webm_medium',
-        :format => 'webm',
-        :path => video_file.webm_medium.path,
-        :size => '1280x720',
-        :quality => 3,
-        :max_video_bitrate => 2000,
-        :public => 1,
-        :sharpen => true
+        label: 'webm_medium',
+        format: 'webm',
+        path: video_file.webm_medium.path,
+        size: '1280x720',
+        quality: 3,
+        max_video_bitrate: 2000,
+        public: 1,
+        sharpen: true
       }
     end
 
+    def mp4_4k_definition
+      {
+        label: '4k',
+        format: 'mp4',
+        path: video_file.mp4_4k.path,
+        h264_profile: 'main',
+        size: '3840x2160',
+        skip: {
+          min_size: '1921x1080'
+        },
+        public: 1,
+        forced_keyframe_rate: '0.5',
+        tuning: 'film',
+        sharpen: true
+      }
+    end
+
+    def mp4_fullhd_definition
+      {
+        label: 'fullhd',
+        format: 'mp4',
+        path: video_file.mp4_fullhd.path,
+        h264_profile: 'main',
+        size: '1920x1080',
+        skip: {
+          min_size: '1281x720'
+        },
+        public: 1,
+        forced_keyframe_rate: '0.5',
+        tuning: 'film',
+        sharpen: true
+      }
+    end
 
     def mp4_high_definition
       {
-        :label => 'high',
-        :format => 'mp4',
-        :path => video_file.mp4_high.path,
-        :h264_level => 3.1,
-        :max_frame_rate => 30,
-        :max_video_bitrate => 3500,
-        :audio_bitrate => 192,
-        :h264_profile => 'main',
-        :size => '1280x720',
-        :public => 1,
-        :forced_keyframe_rate => '0.5',
-        :tuning => "film",
-        :sharpen => true
+        label: 'high',
+        format: 'mp4',
+        path: video_file.mp4_high.path,
+        h264_level: 3.1,
+        max_frame_rate: 30,
+        max_video_bitrate: 3500,
+        audio_bitrate: 192,
+        h264_profile: 'main',
+        size: '1280x720',
+        public: 1,
+        forced_keyframe_rate: '0.5',
+        tuning: 'film',
+        sharpen: true
       }
     end
 
     def mp4_medium_definition
       {
-        :label => 'medium',
-        :format => 'mp4',
-        :path => video_file.mp4_medium.path,
-        :h264_level => 3.1,
-        :max_frame_rate => 30,
-        :max_video_bitrate => 2000,
-        :audio_bitrate => 128,
-        :h264_profile => 'main',
-        :size => '1280x720',
-        :forced_keyframe_rate => '0.5',
-        :public => 1,
-        :sharpen => true
+        label: 'medium',
+        format: 'mp4',
+        path: video_file.mp4_medium.path,
+        h264_level: 3.1,
+        max_frame_rate: 30,
+        max_video_bitrate: 2000,
+        audio_bitrate: 128,
+        h264_profile: 'main',
+        size: '1280x720',
+        forced_keyframe_rate: '0.5',
+        public: 1,
+        sharpen: true
       }
     end
 
     def mp4_low_definition
       {
-        :label => 'low',
-        :format => 'mp4',
-        :path => video_file.mp4_low.path,
-        :device_profile => 'mobile/legacy',
-        :audio_bitrate => 56,
-        :forced_keyframe_rate => '0.5',
-        :public => 1,
-        :sharpen => true
+        label: 'low',
+        format: 'mp4',
+        path: video_file.mp4_low.path,
+        device_profile: 'mobile/legacy',
+        audio_bitrate: 56,
+        forced_keyframe_rate: '0.5',
+        public: 1,
+        sharpen: true
       }
     end
-
 
     def hls_definitions
       return [] if skip_hls
@@ -120,93 +155,93 @@ module Pageflow
 
     def hls_low_definition
       {
-        :label => 'hls-low',
-        :format => 'ts',
-        :source => 'low',
-        :copy_audio => 'true',
-        :copy_video => 'true',
-        :path => video_file.hls_low.path,
-        :type => 'segmented',
-        :public => 1
+        label: 'hls-low',
+        format: 'ts',
+        source: 'low',
+        copy_audio: 'true',
+        copy_video: 'true',
+        path: video_file.hls_low.path,
+        type: 'segmented',
+        public: 1
       }
     end
 
     def hls_medium_definition
       {
-        :label => 'hls-medium',
-        :format => 'ts',
-        :source => 'medium',
-        :copy_audio => 'true',
-        :copy_video => 'true',
-        :path => video_file.hls_medium.path,
-        :type => 'segmented',
-        :public => 1
+        label: 'hls-medium',
+        format: 'ts',
+        source: 'medium',
+        copy_audio: 'true',
+        copy_video: 'true',
+        path: video_file.hls_medium.path,
+        type: 'segmented',
+        public: 1
       }
     end
 
     def hls_high_definition
       {
-        :label => 'hls-high',
-        :format => 'ts',
-        :source => 'high',
-        :copy_audio => 'true',
-        :copy_video => 'true',
-        :path => video_file.hls_high.path,
-        :type => 'segmented',
-        :public => 1
+        label: 'hls-high',
+        format: 'ts',
+        source: 'high',
+        copy_audio: 'true',
+        copy_video: 'true',
+        path: video_file.hls_high.path,
+        type: 'segmented',
+        public: 1
       }
     end
 
     def hls_playlist_definition
       {
-        :label => 'hls-playlist',
-        :streams => hls_stream_definitions,
-        :type => 'playlist',
-        :path => video_file.hls_playlist.path,
-        :public => 1
+        label: 'hls-playlist',
+        streams: hls_stream_definitions,
+        type: 'playlist',
+        path: video_file.hls_playlist.path,
+        public: 1
       }
     end
 
     def hls_stream_definitions
       [
         {
-          :path => video_file.hls_medium.url(host: :hls_origin, default_protocol: 'http'),
-          :bandwidth => 2250
+          path: video_file.hls_medium.url(host: :hls_origin, default_protocol: 'http'),
+          bandwidth: 2250
         },
         {
-          :path => video_file.hls_low.url(host: :hls_origin, default_protocol: 'http'),
-          :bandwidth => 256
+          path: video_file.hls_low.url(host: :hls_origin, default_protocol: 'http'),
+          bandwidth: 256
         },
         {
-          :path => video_file.hls_high.url(host: :hls_origin, default_protocol: 'http'),
-          :bandwidth => 3750
+          path: video_file.hls_high.url(host: :hls_origin, default_protocol: 'http'),
+          bandwidth: 3750
         }
       ]
     end
 
     def smil_definition
       {
-        :streams => smil_stream_definitions,
-        :type => 'playlist',
-        :format => 'highwinds',
-        :path =>  video_file.smil.path,
-        :public => true
+        streams: smil_stream_definitions,
+        type: 'playlist',
+        format: 'highwinds',
+        path: video_file.smil.path,
+        public: true
       }
     end
 
     def smil_stream_definitions
       [
         {
-          :path => video_file.mp4_medium.url(host: :hls_origin, default_protocol: 'http'),
-          :bandwidth => 2250
+          path: video_file.mp4_medium.url(host: :hls_origin, default_protocol: 'http'),
+          bandwidth: 2250
         },
         {
-          :path => video_file.mp4_low.url(host: :hls_origin, default_protocol: 'http'),
-          :bandwidth => 256
+          path: video_file.mp4_low.url(host: :hls_origin, default_protocol: 'http'),
+          bandwidth: 256
         },
         {
-          :path => video_file.mp4_high.url(host: :hls_origin, default_protocol: 'http'),
-          :bandwidth => 3750
+          path: video_file.mp4_high.url(host: :hls_origin, default_protocol: 'http'),
+          bandwidth: 3750
         }
       ]
     end
@@ -223,24 +258,20 @@ module Pageflow
 
     def thumbnails_definition(url_helper)
       {
-        :thumbnails => [
-          with_credentials({
-                             :label => 'poster',
-                             :format => video_file.zencoder_poster.format,
-                             :number => 1,
-                             :start_at_first_frame => true,
-                             :filename => video_file.zencoder_poster.base_name_pattern,
-                             :base_url => url_helper.call(video_file.zencoder_poster.dir_name),
-                             :public => 1
-                           }),
-          with_credentials({
-                             :label => 'thumbnail',
-                             :format => video_file.zencoder_thumbnail.format,
-                             :number => 1,
-                             :filename => video_file.zencoder_thumbnail.base_name_pattern,
-                             :base_url => url_helper.call(video_file.zencoder_thumbnail.dir_name),
-                             :public => 1
-                           })
+        thumbnails: [
+          with_credentials(label: 'poster',
+                           format: video_file.zencoder_poster.format,
+                           number: 1,
+                           start_at_first_frame: true,
+                           filename: video_file.zencoder_poster.base_name_pattern,
+                           base_url: url_helper.call(video_file.zencoder_poster.dir_name),
+                           public: 1),
+          with_credentials(label: 'thumbnail',
+                           format: video_file.zencoder_thumbnail.format,
+                           number: 1,
+                           filename: video_file.zencoder_thumbnail.base_name_pattern,
+                           base_url: url_helper.call(video_file.zencoder_thumbnail.dir_name),
+                           public: 1)
         ]
       }
     end
