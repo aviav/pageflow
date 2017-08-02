d9637534706c3bb752ee259d49e281c7fc82bf7d
Tobias Fankhänel <tfankhaenel@codevise.de>
2017-08-02 20:24:05 +0200

    Fix invite user

--- app/models/pageflow/invited_user.rb
+++ app/models/pageflow/invited_user.rb
@@ -3,7 +3,7 @@ module Pageflow
   # users admin.
   class InvitedUser < User
     before_create :prepare_invitation
-    after_create :send_invitation
+    after_create :create_membership_and_send_invitation
 
     def send_invitation!
       prepare_invitation
@@ -28,6 +28,21 @@ module Pageflow
 
     def password_required?
       false
+    end
+
+    def create_membership_and_send_invitation
+      create_membership
+      send_invitation
+    end
+
+    def create_membership
+      unless accounts.any? &&
+             !Pageflow.config.allow_multiaccount_users
+        Membership.create(entity_id: initial_account.id,
+                          entity_type: 'Pageflow::Account',
+                          user_id: id,
+                          role: initial_role || 'member')
+      end
     end
 
     def send_invitation
