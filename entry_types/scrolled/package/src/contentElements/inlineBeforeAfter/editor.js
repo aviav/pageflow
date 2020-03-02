a7084b2932789720f0df610abafbfb15cfa3f852
Tobias Fankhänel <tfankhaenel@codevise.de>
2020-03-02 17:56:57 +0100

    Add editor inputs
    REDMINE-17372

--- entry_types/scrolled/package/src/contentElements/inlineBeforeAfter/editor.js
+++ entry_types/scrolled/package/src/contentElements/inlineBeforeAfter/editor.js
@@ -1,8 +1,30 @@
 import {editor} from 'pageflow-scrolled/editor';
+import {ColorInputView, FileInputView} from 'pageflow/editor';
+import {CheckBoxInputView, TextInputView} from 'pageflow/ui';
 
 editor.contentElementTypes.register('inlineBeforeAfter', {
   configurationEditor() {
     this.tab('general', function() {
+      this.input('left_id', FileInputView, {
+        collection: 'image_files',
+        fileSelectionHandler: 'contentElementConfiguration',
+      });
+      this.input('left_alt_text', TextInputView);
+      this.input('left_label', TextInputView);
+      this.input('right_id', FileInputView, {
+        collection: 'image_files',
+        fileSelectionHandler: 'contentElementConfiguration',
+      });
+      this.input('right_alt_text', TextInputView);
+      this.input('right_label', TextInputView);
+      this.input('vertical', CheckBoxInputView);
+      this.input('slider', CheckBoxInputView);
+      this.input('slider_handle', CheckBoxInputView, {
+        visibleBinding: 'slider',
+      });
+      this.input('slider_color', ColorInputView, {
+        visibleBinding: 'slider',
+      });
     });
   }
 });
